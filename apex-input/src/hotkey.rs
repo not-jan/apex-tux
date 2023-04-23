c019cfbdae7106603ed8b6a02b129d1b11740437
Kylian Fernandez <kylian2004@free.fr>
2023-04-23 16:39:23 +0200

    update the hotkey library (#27) (#28)

--- apex-input/src/hotkey.rs
+++ apex-input/src/hotkey.rs
@@ -1,42 +1,37 @@
 use crate::Command;
 use anyhow::Result;
-use tauri_hotkey::{Hotkey, HotkeyManager, Key, Modifier};
+use global_hotkey::{GlobalHotKeyManager, hotkey::{HotKey, Modifiers, Code}, GlobalHotKeyEvent};
 use tokio::sync::broadcast;
 
 pub struct InputManager {
-    _hkm: HotkeyManager,
+    _hkm: GlobalHotKeyManager,
 }
 
 impl InputManager {
     pub fn new(sender: broadcast::Sender<Command>) -> Result<Self> {
-        let mut hkm = HotkeyManager::new();
+        let hkm = GlobalHotKeyManager::new().unwrap();
 
-        let modifiers = vec![Modifier::ALT, Modifier::SHIFT];
+        let modifiers = Some(Modifiers::ALT | Modifiers::SHIFT);
 
-        let sender2 = sender.clone();
+        let hotkey_previous = HotKey::new (modifiers, Code::KeyA);
+        let hotkey_next = HotKey::new (modifiers, Code::KeyD);
 
-        hkm.register(
-            Hotkey {
-                modifiers: modifiers.clone(),
-                keys: vec![Key::A],
-            },
-            move || {
+        hkm.register(hotkey_previous).unwrap();
+        hkm.register(hotkey_next).unwrap();
+
+        let hotkey_handler = move|event: GlobalHotKeyEvent| {
+            if event.id == hotkey_previous.id() {
                 sender
                     .send(Command::PreviousSource)
                     .expect("Failed to send command!");
-            },
-        )?;
-        hkm.register(
-            Hotkey {
-                modifiers,
-                keys: vec![Key::D],
-            },
-            move || {
-                sender2
+            }else{
+                sender
                     .send(Command::NextSource)
                     .expect("Failed to send command!");
-            },
-        )?;
+            }
+        };
+
+        GlobalHotKeyEvent::set_event_handler(Some(hotkey_handler));
 
         Ok(Self { _hkm: hkm })
     }
