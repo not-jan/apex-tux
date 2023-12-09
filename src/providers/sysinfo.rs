3537524576c91d042fe842f22470463418f34efb
Lucas Granberg <lukkeg@gmail.com>
2023-12-10 00:49:22 +0200

    change to iso_8859_15 for all fonts

--- src/providers/sysinfo.rs
+++ src/providers/sysinfo.rs
@@ -9,10 +9,10 @@ use num_traits::{pow, Pow};
 
 use config::Config;
 use embedded_graphics::{
-    primitives::{Rectangle, Primitive, PrimitiveStyle},
     geometry::Point,
-    mono_font::{ascii, MonoTextStyle},
+    mono_font::{iso_8859_15, MonoTextStyle},
     pixelcolor::BinaryColor,
+    primitives::{Primitive, PrimitiveStyle, Rectangle},
     text::{renderer::TextRenderer, Baseline, Text},
     Drawable,
 };
@@ -25,9 +25,8 @@ use tokio::{
 };
 
 use sysinfo::{
+    ComponentExt, CpuExt, CpuRefreshKind, NetworkData, NetworkExt, NetworksExt, RefreshKind,
     System, SystemExt,
-    RefreshKind, CpuRefreshKind,
-    CpuExt, NetworkData, NetworkExt, NetworksExt, ComponentExt
 };
 
 #[doc(hidden)]
@@ -43,47 +42,57 @@ fn tick() -> i64 {
 fn register_callback(config: &Config) -> Result<Box<dyn ContentWrapper>> {
     info!("Registering Sysinfo display source.");
 
-    let refreshes = RefreshKind::new().
-        with_cpu(CpuRefreshKind::everything()).
-        with_components_list().
-        with_components().
-        with_networks_list().
-        with_networks().
-        with_memory();
+    let refreshes = RefreshKind::new()
+        .with_cpu(CpuRefreshKind::everything())
+        .with_components_list()
+        .with_components()
+        .with_networks_list()
+        .with_networks()
+        .with_memory();
     let sys = System::new_with_specifics(refreshes);
 
     let tick = tick();
     let last_tick = 0;
 
+    let net_interface_name = config
+        .get_str("sysinfo.net_interface_name")
+        .unwrap_or("eth0".to_string());
 
-	let net_interface_name = config.get_str("sysinfo.net_interface_name").unwrap_or("eth0".to_string());
+    if sys
+        .networks()
+        .iter()
+        .find(|(name, _)| **name == net_interface_name)
+        .is_none()
+    {
+        warn!("Couldn't find network interface `{}`", net_interface_name);
+        info!("Instead, found those interfaces:");
+        for (interface_name, _) in sys.networks() {
+            info!("\t{}", interface_name);
+        }
+    }
 
-	if sys.networks().iter().find(|(name, _)|
-		**name == net_interface_name
-	).is_none(){
-		warn!("Couldn't find network interface `{}`", net_interface_name);
-		info!("Instead, found those interfaces:");
-		for (interface_name, _) in sys.networks() {
-			info!("\t{}", interface_name);
-		}
-	}
+    let sensor_name = config
+        .get_str("sysinfo.sensor_name")
+        .unwrap_or("hwmon0 CPU Temperature".to_string());
 
-
-	let sensor_name = config.get_str("sysinfo.sensor_name").unwrap_or("hwmon0 CPU Temperature".to_string());
-
-	if sys.components().iter().find(|component|
-		component.label() == sensor_name
-	).is_none() {
-		warn!("Couldn't find sensor `{}`", sensor_name);
-		info!("Instead, found those sensors:");
-		for component in sys.components() {
-			info!("\t{:?}", component);
-		}
-		
-	}
+    if sys
+        .components()
+        .iter()
+        .find(|component| component.label() == sensor_name)
+        .is_none()
+    {
+        warn!("Couldn't find sensor `{}`", sensor_name);
+        info!("Instead, found those sensors:");
+        for component in sys.components() {
+            info!("\t{:?}", component);
+        }
+    }
 
     Ok(Box::new(Sysinfo {
-        sys, tick, last_tick, refreshes,
+        sys,
+        tick,
+        last_tick,
+        refreshes,
         polling_interval: config.get_int("sysinfo.polling_interval").unwrap_or(2000) as u64,
         net_load_max: config.get_float("sysinfo.net_load_max").unwrap_or(100.0),
         cpu_frequency_max: config.get_float("sysinfo.cpu_frequency_max").unwrap_or(7.0),
@@ -121,34 +130,71 @@ impl Sysinfo {
         let mut buffer = FrameBuffer::new();
 
         self.render_stat(0, &mut buffer, format!("C: {:>4.0}%", load), load / 100.0)?;
-        self.render_stat(1, &mut buffer, format!("F: {:>4.2}G", freq), freq / self.cpu_frequency_max)?;
-        self.render_stat(2, &mut buffer, format!("M: {:>4.1}G", mem_used), self.sys.used_memory() as f64 / self.sys.total_memory() as f64)?;
+        self.render_stat(
+            1,
+            &mut buffer,
+            format!("F: {:>4.2}G", freq),
+            freq / self.cpu_frequency_max,
+        )?;
+        self.render_stat(
+            2,
+            &mut buffer,
+            format!("M: {:>4.1}G", mem_used),
+            self.sys.used_memory() as f64 / self.sys.total_memory() as f64,
+        )?;
 
-        if let Some(n) = self.sys.networks().iter().find(|(name, _)|
-            **name == self.net_interface_name
-        ).map(|t| t.1){
-            let net_direction = if n.received() > n.transmitted() {"I"} else {"O"};
+        if let Some(n) = self
+            .sys
+            .networks()
+            .iter()
+            .find(|(name, _)| **name == self.net_interface_name)
+            .map(|t| t.1)
+        {
+            let net_direction = if n.received() > n.transmitted() {
+                "I"
+            } else {
+                "O"
+            };
 
             let (net_load, net_load_power, net_load_unit) = self.calculate_max_net_rate(n);
-            let mut adjusted_net_load = format!("{:.4}", (net_load / 1024_f64.pow(net_load_power)).to_string());
+            let mut adjusted_net_load = format!(
+                "{:.4}",
+                (net_load / 1024_f64.pow(net_load_power)).to_string()
+            );
 
             if adjusted_net_load.ends_with(".") {
                 adjusted_net_load = adjusted_net_load.replace(".", "");
             }
 
-            let _ = self.render_stat(3, &mut buffer, format!("{}: {:>4}{}", net_direction, adjusted_net_load, net_load_unit), net_load / (self.net_load_max * 1024_f64.pow(2)));
+            let _ = self.render_stat(
+                3,
+                &mut buffer,
+                format!(
+                    "{}: {:>4}{}",
+                    net_direction, adjusted_net_load, net_load_unit
+                ),
+                net_load / (self.net_load_max * 1024_f64.pow(2)),
+            );
         };
 
-		if let Some(c) = self.sys.components().iter().find(|component|
-            component.label() == self.sensor_name
-        ){
-            let _ = self.render_stat(4, &mut buffer, format!("T: {:>4.1}C", c.temperature()), c.temperature() as f64 / self.temperature_max);
-		}
+        if let Some(c) = self
+            .sys
+            .components()
+            .iter()
+            .find(|component| component.label() == self.sensor_name)
+        {
+            let _ = self.render_stat(
+                4,
+                &mut buffer,
+                format!("T: {:>4.1}C", c.temperature()),
+                c.temperature() as f64 / self.temperature_max,
+            );
+        }
 
         Ok(buffer)
     }
 
-    fn calculate_max_net_rate(&self, net : &NetworkData) -> (f64, i32, &str) {
+    fn calculate_max_net_rate(&self, net: &NetworkData) -> (f64, i32, &str) {
         let max_diff = std::cmp::max(net.received(), net.transmitted()) as f64;
         let max_rate = max_diff / ((self.tick - self.last_tick) as f64 / 1000.0);
 
@@ -156,7 +202,7 @@ impl Sysinfo {
             r if r > 1024_f64.pow(3) => (r, 3, "G"),
             r if r > 1024_f64.pow(2) => (r, 2, "M"),
             r if r > 1024_f64.pow(1) => (r, 1, "k"),
-            r => (r, 0, "B")
+            r => (r, 0, "B"),
         }
     }
 
@@ -167,38 +213,39 @@ impl Sysinfo {
         self.tick = tick();
     }
 
-    fn render_stat(&self, slot: i32, buffer: &mut FrameBuffer, text : String, fill : f64) -> Result<()> {
-        let style = MonoTextStyle::new(&ascii::FONT_4X6, BinaryColor::On);
+    fn render_stat(
+        &self,
+        slot: i32,
+        buffer: &mut FrameBuffer,
+        text: String,
+        fill: f64,
+    ) -> Result<()> {
+        let style = MonoTextStyle::new(&iso_8859_15::FONT_4X6, BinaryColor::On);
         let metrics = style.measure_string(&text, Point::zero(), Baseline::Top);
 
-        let slot_y = slot*8 + 1;
+        let slot_y = slot * 8 + 1;
 
-        Text::with_baseline(
-            &text,
-            Point::new(0, slot_y),
-            style,
-            Baseline::Top,
-        )
-        .draw(buffer)?;
+        Text::with_baseline(&text, Point::new(0, slot_y), style, Baseline::Top).draw(buffer)?;
 
         let bar_start: i32 = metrics.bounding_box.size.width as i32 + 2;
         let border_style = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
         let fill_style = PrimitiveStyle::with_fill(BinaryColor::On);
-        let fill_width = if fill.is_infinite() { 0 } else {
+        let fill_width = if fill.is_infinite() {
+            0
+        } else {
             (fill * (127 - bar_start) as f64).floor() as i32
         };
 
-        Rectangle::with_corners(
-            Point::new(bar_start, slot_y),
-            Point::new(127, slot_y + 6)
-        ).into_styled(border_style)
-         .draw(buffer)?;
+        Rectangle::with_corners(Point::new(bar_start, slot_y), Point::new(127, slot_y + 6))
+            .into_styled(border_style)
+            .draw(buffer)?;
 
         Rectangle::with_corners(
             Point::new(bar_start + 1, slot_y + 1),
-            Point::new(bar_start + fill_width, slot_y + 5)
-        ).into_styled(fill_style)
-         .draw(buffer)?;
+            Point::new(bar_start + fill_width, slot_y + 5),
+        )
+        .into_styled(fill_style)
+        .draw(buffer)?;
 
         Ok(())
     }
