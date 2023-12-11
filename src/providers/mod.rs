d53e30a5f7c656e046e1612e839ac2427eff73e5
not-jan <61017633+not-jan@users.noreply.github.com>
2023-12-11 18:16:02 +0100

    Fix formatting (#54)

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,9 +1,9 @@
 pub(crate) mod clock;
 #[cfg(feature = "crypto")]
 pub(crate) mod coindesk;
+#[cfg(feature = "image")]
+pub(crate) mod image;
 #[cfg(any(feature = "dbus-support", target_os = "windows"))]
 pub(crate) mod music;
 #[cfg(feature = "sysinfo")]
 pub(crate) mod sysinfo;
-#[cfg(feature = "image")]
-pub(crate) mod image;
